234b4f44293b3aed3f6bdd59dbb4df10603d6175
Mary Haedrich <mary@Marys-Mac-mini.local>
2022-01-21 17:37:50 -0500

    added projects

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -15,28 +15,40 @@
 
     <section class="py-8">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                <a href="/" target="_blank" class="flex flex-col justify-center items-center gap-2 hover:transform hover:scale-110 transition ease-out duration-300">
-                    <div class="w-72 h-64 bg-dark dark:bg-light"></div>
+                <a href="https://rickandmortypedia.vercel.app/" target="_blank" class="flex flex-col justify-center items-center gap-2 hover:transform hover:scale-110 transition ease-out duration-300">
+                    <div class="w-72 h-72 bg-dark dark:bg-light">
+                        <img src="/rickandmortypedia-cover.png" alt="RickandMortyPedia Cover Art" />
+                    </div>
                 </a>
 
                 <a href="/" target="_blank" class="flex flex-col justify-center items-center gap-2 hover:transform hover:scale-110 transition ease-out duration-300">
-                    <div class="w-72 h-64 bg-dark dark:bg-light"></div>
+                    <div class="w-72 h-72">
+                        <img src="/preview.png" alt="Coming Soon" />
+                    </div>
+                </a>
+
+                <a href="https://simple-sveltekit-starter.vercel.app/" target="_blank" class="flex flex-col justify-center items-center gap-2 hover:transform hover:scale-110 transition ease-out duration-300">
+                    <div class="w-72 h-72 bg-dark dark:bg-light">
+                        <img src="/simplesveltekit-cover.png" alt="Simple SvelteKit Starter Cover Art" />
+                    </div>
                 </a>
 
                 <a href="/" target="_blank" class="flex flex-col justify-center items-center gap-2 hover:transform hover:scale-110 transition ease-out duration-300">
-                    <div class="w-72 h-64 bg-dark dark:bg-light"></div>
+                    <div class="w-72 h-72">
+                        <img src="/preview.png" alt="Coming Soon" />
+                    </div>
                 </a>
 
                 <a href="/" target="_blank" class="flex flex-col justify-center items-center gap-2 hover:transform hover:scale-110 transition ease-out duration-300">
-                    <div class="w-72 h-64 bg-dark dark:bg-light"></div>
+                    <div class="w-72 h-72">
+                        <img src="/mycelium-cover.png" alt="Mycelium Cover Art" />
+                    </div>
                 </a>
 
                 <a href="/" target="_blank" class="flex flex-col justify-center items-center gap-2 hover:transform hover:scale-110 transition ease-out duration-300">
-                    <div class="w-72 h-64 bg-dark dark:bg-light"></div>
-                </a>
-
-                <a href="/" target="_blank" class="flex flex-col justify-center items-center gap-2 hover:transform hover:scale-110 transition ease-out duration-300">
-                    <div class="w-72 h-64 bg-dark dark:bg-light"></div>
+                    <div class="w-72 h-72 bg-dark dark:bg-light">
+                        <img src="/preview.png" alt="Coming Soon" />
+                    </div>
                 </a>                
             </div>
     </section>
