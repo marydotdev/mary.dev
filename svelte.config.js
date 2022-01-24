dced32082d68f175d2fbaf56aeda9b0cfaada6e4
Mary Haedrich <mary@Marys-Mac-mini.local>
2022-01-24 17:24:41 -0500

    add vercel adapter again

--- svelte.config.js
+++ svelte.config.js
@@ -21,6 +21,12 @@ const config = {
     target: '#svelte',
     adapter: vercel(),
 
+    // if you are not using the static adapter and
+    // you don't want prerendering, remove this section
+    prerender: {
+      entries: ['*', '/sitemap.xml', '/rss.xml']
+    },
+
     vite: {
       // allows vite access to ./posts
       server: {
