31ce94fc1c77f2c48a88153b863854ee26b7755e
Mary Haedrich <mary@Marys-Mac-mini.local>
2022-01-25 17:01:05 -0500

    update project pages

--- src/routes/projects/[slug].svelte
+++ src/routes/projects/[slug].svelte
@@ -22,6 +22,9 @@
               fileName
               url
             }
+            tags
+            source
+            link
           }
         }
       `
@@ -44,29 +47,45 @@
 <script>
   import { name, website } from '$lib/info'
   
-  export let project
+  export let project;
+
+  let tags = project.tags;
 </script>
 
 <svelte:head>
   <title>{name} | {project.title}</title>
 </svelte:head>
   
-<div class="max-w-4xl mx-auto">
+<div class="max-w-4xl mx-auto font-display">
 
   <h1 class="text-3xl sm:text-5xl font-regular tracking-tight pb-4 sm:pb-8">{project.title}</h1>
   <div class="grid grid-cols-1 sm:grid-cols-2">
-  <img src="{project.coverImage.url}" alt="{project.coverImage.fileName}" class="" />
-  <div>
-    <p>{project.description}</p>
+    <img src="{project.coverImage.url}" alt="{project.coverImage.fileName}" class="" />
+    <div class="flex flex-col justify-around sm:pl-8">
+      <div class="py-4">
+        <h2 class="text-2xl sm:text-4xl font-regular tracking-tight pb-4">The Stack</h2>
+        <ul class="flex flex-col">
+          {#each tags as tag}
+          <li class="text-xl">{tag}</li>
+          {/each}
+        </ul>
+      </div>
+      <div class="py-4">
+        <h2 class="text-2xl sm:text-4xl font-regular tracking-tight pb-4">The Project</h2>
+        <p class="text-xl">{@html project.text.html}</p>
+      </div>
+      <div class="flex flex-col gap-2 py-4">
+        <a href="{project.link}" target="_blank" class="text-2xl sm:text-3xl font-regular tracking-tight">Visit Site</a>
+        <a href="{project.source}" target="_blank" class="text-2xl sm:text-3xl font-regular tracking-tight">View Source</a>
+      </div>
+    </div>
   </div>
-  </div>
-  <article class="prose prose-zinc prose-md sm:prose sm:prose-zinc sm:prose-lg sm:max-w-none dark:prose-invert">
-    {@html project.text.html}
-  </article>
+
+    
 
   <div class="pt-12 flex justify-end">
     <a href={`/projects`} class="text-xl sm:text-2xl hover:font-medium transition duration-500">
-      Back to Projects
+      Back to Projects &rarr;
     </a>
   </div>
 </div>
