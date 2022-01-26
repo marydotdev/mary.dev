0ad5b21b5f1840ee0c09e8060ca9e206081c3f3b
Mary Haedrich <mary@Marys-Mac-mini.local>
2022-01-26 17:41:00 -0500

    add width to hero image, add meta tags

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # mary.dev
-
-My personal website built on Matt Jennings awesome [SvelteKit Blog Template](https://sveltekit-blog-template.vercel.app).
--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,21 +4,21 @@
 
 <footer class="pt-12 pb-4 font-display text-center bottom-0">
         <div class="flex justify-center gap-8 py-4">
-            <a href="https://twitter.com/marydotdev" class="hover:transform hover:scale-110 transition ease-out duration-300">
+            <a href="https://twitter.com/marydotdev" alt="twitter.com/marydotdev" class="hover:transform hover:scale-110 transition ease-out duration-300">
                 <TwitterIcon size="1.5x" />
             </a>
-            <a href="https://github.com/marydotdev" class="hover:transform hover:scale-110 transition ease-out duration-300">
+            <a href="https://github.com/marydotdev" alt="github.com/marydotdev" class="hover:transform hover:scale-110 transition ease-out duration-300">
                 <GithubIcon size="1.5x" />
             </a>
-            <a href="https://youtube.com/marydotdev" class="hover:transform hover:scale-110 transition ease-out duration-300">
+            <a href="https://youtube.com/marydotdev" alt="youtube.com/marydotdev" class="hover:transform hover:scale-110 transition ease-out duration-300">
                 <YoutubeIcon size="1.5x" />
             </a>
-            <a href="mailto:mary.haedrich@gmail.com" class="hover:transform hover:scale-110 transition ease-out duration-300">
+            <a href="mailto:mary.haedrich@gmail.com" alt="email" class="hover:transform hover:scale-110 transition ease-out duration-300">
                 <MailIcon size="1.5x" />
             </a>
-            <a href="/" class="hover:transform hover:scale-110 transition ease-out duration-300">
+            <a href="/" alt="rss" class="hover:transform hover:scale-110 transition ease-out duration-300">
                 <RssIcon size="1.5x" />
             </a>
         </div>
-    <a href="https://twitter.com/marydotdev" class="text-md font-display tracking-tight opacity-10 hover:opacity-80 transition ease-out duration-300">Made by Mary</a>
+    <a href="https://twitter.com/marydotdev" class="text-md font-display tracking-tight opacity-70 hover:opacity-100 transition ease-out duration-300">Made by Mary</a>
 </footer>
