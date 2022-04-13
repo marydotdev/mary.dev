2b23f335378562d0538afbc2615b3d0e04232a38
Mary Haedrich <mary@Marys-Mac-mini.local>
2022-04-12 22:53:16 -0400

    remove external link cursor alias

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,5 +4,5 @@ import SocialLinks from "./SocialLinks.svelte";
 </script>
 <footer class="pt-12 pb-4 font-inter flex flex-col items-center justify-center text-center bottom-0">
     <SocialLinks />
-    <a href="https://twitter.com/marydotdev" class="font-inter opacity-70 sm:hover:opacity-100 transition ease-out duration-300 sm:hover:cursor-alias">Made by Mary</a>
+    <a href="https://twitter.com/marydotdev" class="font-inter opacity-70 sm:hover:opacity-100 transition ease-out duration-300">Made by Mary</a>
 </footer>
